7e6b051ac3d0be062e03d6886fb3e152d4dcabd0
Jimi <jimi.herk@schule.local>
2024-08-18 21:44:24 -0700

    🚀 Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+
+if (!fs.existsSync('logs.txt')) {
+    fs.writeFileSync('logs.txt', '');
+}
+
+if (!fs.existsSync('allow.txt')) {
+    fs.writeFileSync('allow.txt', '');
+}
+
+let allowed = fs.readFileSync('allow.txt', 'utf-8')
+    .split('\n');
+
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    },
+    authStrategy: new LocalAuth(),
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('remote_session_saved', () => {
+    console.log('The session has been saved!');
+});
+
+
+client.on('call', async call => {
+    if (!allowed.includes(call.from)) {
+        await call.reject();
+        fs.appendFileSync('logs.txt', `Call from ${call.from} has been rejected\n`);
+        await client.sendMessage(call.from, `I do not take calls on WhatsApp. Please call me on FaceTime or send me a message. Thanks! (This is an automated message)`);
+        fs.appendFileSync('logs.txt', `Automated message sent to ${call.from}\n`);
+    }
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.initialize();
